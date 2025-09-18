19fd984ec9a1d16d5ce3048ee011cfcb438b17ec
Ivanoskif <filipivanoski08@gmail.com>
2025-09-18 23:41:38 +0200

    Testing CD

--- frontend/src/pages/ListPage.tsx
+++ frontend/src/pages/ListPage.tsx
@@ -17,7 +17,7 @@ export default function ListPage({ items, onDelete, onToggleFavorite }: Props) {
         <div>
           <h2 style={{margin:'0 0 6px'}}>All Cocktails</h2>
         </div>
-        <Link to="/new" className="btn primary">+ Add Cocktail</Link>
+        <Link to="/new" className="btn primary">+ Add</Link>
       </div>
 
       <CocktailList items={items} onDelete={onDelete} onToggleFavorite={onToggleFavorite} />
